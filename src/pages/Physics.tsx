4546e9c3d043a5c11ddd40b48bd7a051a6d83776
Muslim121 <tulegenov_muslim@mail.ru>
2024-03-10 21:21:54 +0500

    SDfsF

--- src/pages/Physics.tsx
+++ src/pages/Physics.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import Asside from "../components/Asside";
 import Notfound from "../components/Notfound";
+import { useAppSelector } from "../redux/hooks";
 
 const Physics: React.FC = () => {
+  const openValue = useAppSelector((state) => state.open.openValue);
+
   return (
     <div className=" flex">
       <Asside />
-      <div className=" mx-auto mt-2">
-        <Notfound />
-      </div>
+      {openValue && (
+        <div className=" mx-auto mt-2">
+          <Notfound />
+        </div>
+      )}
     </div>
   );
 };
